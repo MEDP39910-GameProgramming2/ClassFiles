c497f2d58b42b5158d4dc7e7769b92e95da0e4b5
jcontour <contj186@newschool.edu>
2019-02-28 21:35:58 -0500

    sprites & ui

---
+++ 04_Sprites_UI/Assets/CoinCollect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollect : MonoBehaviour
+{
+    public Text scoretext;
+    public Slider slideymcslidey;
+    int score = 0;
+
+    private void Start()
+    {
+        scoretext.text = "score = " + score.ToString();
+        slideymcslidey.value = score;
+    }
+
+    private void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        if (otherObject.gameObject.tag == "coin") {
+            score++;
+
+            slideymcslidey.value = score;
+
+            if (score == 4) {
+                scoretext.text = "WINNER";
+            } else {
+                scoretext.text = "score = " + score.ToString();
+            }
+
+            Destroy(otherObject.gameObject);
+            Debug.Log(score);
+        }
+    }
+
+}
