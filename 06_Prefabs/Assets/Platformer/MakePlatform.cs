1c10fd168c6654c347a8fc57b447fa985cd28981
jcontour <contj186@newschool.edu>
2019-03-15 11:08:41 -0400

    prefabs

---
+++ 06_Prefabs/Assets/Platformer/MakePlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePlatform : MonoBehaviour
+{
+
+    public GameObject platform_prefab;
+    float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 2) {
+            GameObject p;
+            p = Instantiate(platform_prefab, new Vector2(Random.Range(-9, 9), -6), Quaternion.identity);
+
+            p.transform.localScale = new Vector2(Random.Range(3,8), 0.2f);
+
+            timer = 0;
+        }
+
+    }
+}
