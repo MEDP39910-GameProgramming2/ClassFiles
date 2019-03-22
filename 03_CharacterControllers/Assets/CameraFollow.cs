53367c19cdc23fc4107a05823c82d5470f52cf99
jcontour <contj186@newschool.edu>
2019-03-21 21:45:00 -0400

    drag and drop

---
+++ 03_CharacterControllers/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = player.transform.position;
+        transform.position = new Vector3(pos.x, pos.y, -10);
+        
+    }
+}
